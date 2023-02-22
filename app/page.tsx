8f63e28b988b509c94ad9f2cbe10107082bf958f
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2023-02-22 22:06:14 +0100

    Works but doesn't work

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import useElevator from "./useElevator"
 
 
 export default function Home() {
-  const { currentFloor, floors, count, callElevator, registerFloor, moveElevator } = useElevator(10)
+  const { count, currentFloorIndex, floors, registerFloor, requestElevator } = useElevator(10)
 
   function submitRegister(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     const registeredFloor = Number((
       (e.target as HTMLFormElement)
-      .querySelector("input[type='submit']:focus") as HTMLInputElement
+        .querySelector("input[type=\"submit\"]:focus") as HTMLInputElement
     ).value)
 
     registerFloor(registeredFloor)
@@ -22,16 +22,16 @@ export default function Home() {
         <tbody>
           {floors.map((floor, i) => (
             <tr key={i}>
-              <td><button onClick={() => callElevator(i)}>Call to floor {i}</button></td>
+              <td><button onClick={() => requestElevator(i)}>Request to floor {i}</button></td>
               <td>{floor.waiting}</td>
-              <td className ={`elevator ${currentFloor === i ? "current" : ""}`} data-count={count} />
+              <td className={`elevator ${currentFloorIndex === i ? "current" : ""}`} data-count={count} />
             </tr>
           ))}
         </tbody>
       </table>
       <form onSubmit={submitRegister}>
         {floors.map((_, i) => (
-          <input key={i} type="submit" value={i} disabled={currentFloor === i} />
+          <input key={i} type="submit" value={i} disabled={currentFloorIndex === i} />
         ))}
       </form>
     </main>
--- app/useElevator.ts
+++ app/useElevator.ts
@@ -1,37 +1,108 @@
-"use client"
-
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 export default function useElevator(floorCount: number) {
-  const [ currentFloor, setCurrentFloor ] = useState(0)
-  const [ floors, setFloors ] = useState(Array.from({ length: floorCount }, (_, i) => ({
-    waiting: 0,
-    leaving: 0,
-  })))
-  const count = floors.reduce((count, { leaving }) => count + leaving, 0)
+  const [ currentFloorIndex, setCurrentFloorIndex ] = useState(0)
+  const [ floors, setFloors ] = useState(Array.from({ length: floorCount }, _ => ({ leaving: 0, waiting: 0 })))
+  const [ direction, setDirection ] = useState<"down" | "up" | null>(null)
+  const currentFloor = floors[currentFloorIndex]
 
-  function callElevator(floor: number) {
-    const newFloors = [ ...floors ]
+  function requestElevator(floor: number) {
+    const updatedFloors = [ ...floors ]
+    updatedFloors[floor].waiting += 1
 
-    newFloors[floor].waiting = floors[floor].waiting + 1
+    setFloors(updatedFloors)
 
-    setFloors(newFloors)
+    if(currentFloorIndex === floor || direction !== null) return
+
+    start(floor)
   }
 
   function registerFloor(floor: number) {
-    /* 
-      When people in current floor waiting click register, the
-      waiting number decrements by 1 and leaving to the floor increments
-      by 1 etc.
-    */
+    if(currentFloor.waiting === 0) return
+
+    const updatedFloors = [ ...floors ]
+    updatedFloors[floor].leaving += 1
+    updatedFloors[currentFloorIndex].waiting -= 1
+
+    setFloors(updatedFloors)
+
+    if(currentFloor.waiting === 0) start(floor)
   }
 
-  async function moveElevator(floor: number) {
+  async function moveElevator(): Promise<void> {
+    const destination = getNextDestination()
+    console.log(destination)
+    if(destination === -1) return dispatch()
+
+    await delay(500)
+    setCurrentFloorIndex(prev => direction === "down" ? prev + 1 : prev - 1)
+    dispatch()
+  }
+
+  function getNextDestination(): number {
+    // If it's stationary there's no need to get a destination
+    if(direction === null) return -1
+
+    // Get the floors that are above or under current one depending on direction
+    const slicedFloors = floors
+      .slice(
+        direction === "down" ? currentFloorIndex + 1 : 0, 
+        direction === "down" ? floors.length : currentFloorIndex
+      )
+    // Find first occurence of data > 0; reverse-checks if it goes up 
+    const nextDestination = (
+      direction === "down" 
+        ? slicedFloors 
+        : slicedFloors.reverse()
+    ).find(floor => floor.leaving > 0 || floor.waiting > 0)
+
+    if(nextDestination === undefined) return -1
+
+    // If found - return
+    if(nextDestination !== undefined) {
+      return floors.indexOf(nextDestination)
+    }
+
+    // If there is any data > 0 it means it's in the other direction - change direction and repeat
+    if(floors.some(floor => floor.leaving > 0 || floor.waiting > 0)) {
+      setDirection(prev => prev === "down" ? "up" : "down")
+      return getNextDestination()
+    }
+
+    // No destinations = stationary
+    setDirection(null)
+    return -1
+  }
+
+  function start(floor: number) {
+    setDirection(currentFloorIndex < floor ? "down" : "up")
+  }
+
+  function dispatch() {
+    const updatedFloors = [ ...floors ]
+    updatedFloors[currentFloorIndex].leaving = 0
     
+    setFloors(updatedFloors)
   }
 
-  return { currentFloor, floors, count, callElevator, registerFloor, moveElevator }
+  useEffect(() => {
+    if(currentFloor.waiting > 0) {
+      setDirection(null)
+      return
+    }
+    if(direction === null) return
+
+    moveElevator()
+  }, [ currentFloorIndex, direction ])
+
+  return {
+    count: floors.reduce((count, { leaving }) => count + leaving, 0),
+    currentFloorIndex,
+    floors,
+    registerFloor,
+    requestElevator
+  }
 }
 
 function delay(ms: number) {
